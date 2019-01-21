a1f33f5706225fc7610f8d2eb7bb01677ae3f999
mohamedhamtou <mohamed.hamtou@qantasloyalty.com>
2019-01-21 16:09:56 +1100

    README.md

--- src/main/java/com/coffeebeans/mycodeschool/package-info.java
+++ src/main/java/com/coffeebeans/mycodeschool/package-info.java
@@ -202,7 +202,4 @@
  *    limitations under the License.
  */
 
-/**
- * Implementations for algorithms introduced in https://www.youtube.com/user/mycodeschool
- */
 package com.coffeebeans.mycodeschool;
